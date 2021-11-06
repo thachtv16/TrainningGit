45bfdb9f46504f061a996846a0d9fd6c81042f53
Trần Văn Thạch <86756564+thach16@users.noreply.github.com>
2021-11-06 12:20:14 +0700

    save second test

--- app/src/main/java/jp/knotapp/trainninggit/MainActivity.kt
+++ app/src/main/java/jp/knotapp/trainninggit/MainActivity.kt
@@ -2,10 +2,15 @@ package jp.knotapp.trainninggit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun saveSecondTest() {
+        Log.d("thachtv", "saveSecondTest: second test")
     }
 }
